8b4a299d1741b1bc2e64715569217a9efcfe89dc
Alexander Arakelyan <alexander.arakelyan@gmail.com>
2024-12-13 20:14:38 -0500

    assign

---
+++ app_assign.cpp
@@ -0,0 +1,27 @@
+#include <boost/assign.hpp>
+#include <boost/rational.hpp>
+#include <iterator>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    const std::vector<std::string> list1 = boost::assign::list_of("a")("b")("c")("d")("e");
+    for (auto i = list1.begin(); i != list1.end(); i++)
+    {
+        std::cout << " list_of " << (*i) << std::endl;
+    }
+
+    const std::map<std::string, boost::rational<int>> list2 = boost::assign::map_list_of //
+        ("str1", boost::rational<int>(1, 2))                                             //
+        ("str2", boost::rational<int>(1, 3))                                             //
+        ("str3", boost::rational<int>(1, 4));
+
+    for (auto i = list2.begin(); i != list2.end(); i++)
+    {
+        std::cout << " list_of " << (i->first) << " - " << (i->second) << std::endl;
+    }
+
+    std::cout << " sz " << boost::size(list2) << std::endl;
+
+    return 0;
+}
