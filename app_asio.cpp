8fbd44bd31e342ba13a9eb2ec081a8989f78cfe4
Alexander Arakelyan <alexander.arakelyan@gmail.com>
2024-12-23 20:51:07 -0500

    boost asio

--- app_asio.cpp
+++ app_asio.cpp
@@ -4,6 +4,7 @@
 
 int main(int args, char **argv)
 {
+    // io service
     boost::asio::io_service svc;
     svc.dispatch([]()
                  { std::cout << "dispatch" << std::endl; });
@@ -11,5 +12,17 @@ int main(int args, char **argv)
              { std::cout << "post" << std::endl; });
     svc.poll();
     // svc.run();
+
+    // strands
+    boost::asio::io_service svc2;
+    boost::asio::io_service::strand strand(svc2);
+    int counter = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        svc2.post(strand.wrap([&counter, i]()
+                              { std::cout << "strand " << counter++ << " - " << i << std::endl; }));
+    }
+
+    svc2.poll();
     return 0;
 }
--- app_concurrency.cpp
+++ app_concurrency.cpp
@@ -7,10 +7,19 @@
 int main(int argc, char const *argv[])
 {
   std::vector<std::string> v1{"str1", "str2", "str3"};
+
+  // thread
   boost::thread([](const std::vector<std::string> &val1)
                 { 
                     for(auto i=val1.begin(); i!=val1.end();i++) {
                     std::cout << (*i) << std::endl;
                     } }, v1);
+
+  // thread group
+  boost::thread_group pool;
+  pool.create_thread([]()
+                     { std::cout << "pool" << std::endl; });
+  pool.join_all();
+
   return 0;
 }
